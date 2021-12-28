8f5964ef720016dfcfc1a3a87b678859cfb976ae
Ernest Wong <ernestwong1027@gmail.com>
2021-12-28 13:28:21 -0500

    bug fixes

--- src/middleware/responseHandler.ts
+++ src/middleware/responseHandler.ts
@@ -2,6 +2,7 @@ import {Request, Response, NextFunction} from "express";
 
 // Return response based on response data
 export const responseHandler = (responseData: Record<string, any>, request: Request , response: Response, next: NextFunction) => {
+    if(request.body.newAccessToken)responseData.data = {...responseData.data,newAccessToken:request.body.newAccessToken};
     if (responseData.code >=400 ) {
         const responseJSON = {
             status: 0,
--- src/middleware/verifyDriverMiddleware.ts
+++ src/middleware/verifyDriverMiddleware.ts
@@ -1,7 +1,7 @@
 import {Request, Response, NextFunction} from "express";
 
 // Return response based on response data
-export const VerifyDriverMiddleware = (responseData: Record<string, any>, request: Request , response: Response, next: NextFunction) => {
+export const VerifyDriverMiddleware = (request: Request , response: Response, next: NextFunction) => {
    if(request.body.user.isDriver)return next();
     response.status(403).send('Invalid Token')
 }
